b4d4e08d3db3c422f55efd06bbf38b3ab677a355
Mikael Korpela <mikael@ihminen.org>
2020-06-19 01:18:16 +0300

    Updates

---
+++ precache-manifest.30e793b43d521d84af6cf1b957e8a92c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a965efce9d99558bca8d4a7b3f6638b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93e0b771b5580d001f9d",
+    "url": "/static/css/main.bbe1d6ac.chunk.css"
+  },
+  {
+    "revision": "bbaa60ca620d4020721c",
+    "url": "/static/js/2.062740e9.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/static/js/2.062740e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93e0b771b5580d001f9d",
+    "url": "/static/js/main.969d0970.chunk.js"
+  },
+  {
+    "revision": "31aa0cf4383b715cfd7e",
+    "url": "/static/js/runtime-main.5a3fa1b7.js"
+  },
+  {
+    "revision": "ba454e2cb221fa88b11239f8aedfbab2",
+    "url": "/static/media/sofa.ba454e2c.svg"
+  }
+]);
