8d7a001dc33546b76c4107d09d9a22efe62e2092
Mikael Korpela <mikael@ihminen.org>
2020-06-24 20:23:59 +0300

    Add Sentry.io

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/browser';
 
 import './index.scss';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
+
+// JavaScript error logging
+Sentry.init({
+  dsn:
+    'https://dace55ddccfc480083c04f2a876322a3@o356100.ingest.sentry.io/5288459',
+});
 
 ReactDOM.render(
   <React.StrictMode>
